2f37c8ac7561e16a7bd21b7c26d883dd5fc5b173
HankTk <hidenori@comcast.net>
2025-04-10 15:21:18 -0700

    Initial Commit

---
+++ ManageTodo/ManageTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageTodoApp.swift
+//  ManageTodo
+//
+//  Created by Hidenori Takaku on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ManageTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ManageTodoTests/ManageTodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ManageTodoTests.swift
+//  ManageTodoTests
+//
+//  Created by Hidenori Takaku on 4/10/25.
+//
+
+import Testing
+@testable import ManageTodo
+
+struct ManageTodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
